c5d305e8d4ba012ad5bf5c0cc8563adb8b160ba8
olegnovitskiy <oleg.novitskiy.dev@gmail.com>
2017-03-04 01:15:19 +0200

    Initial commit

---
+++ src/com/ocp/intro/Main.java
@@ -0,0 +1,12 @@
+package com.ocp.intro;
+
+public class Main {
+
+    public static void main(String[] args) {
+        float value1 = 102;
+        float value2 = (int)102.0;
+//        float value3 = 1f * 0.0;
+        float value4 = 1f * (short)0.0;
+//        float value5 = 1f * (boolean)0.0;
+    }
+}
